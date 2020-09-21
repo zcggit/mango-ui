fefff857c3f660d8f6cc1207748a876a03922648
zcggit <13641368226@yeah.net>
2020-09-22 00:32:41 +0800

    2222

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,40 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links1111</h2>
+    <h2>Essential Links11</h2>
     <ul>
       <li>
         <a
           href="https://vuejs.org"
           target="_blank"
+          rel ="noopener noreferrer"
         >
-          Core Docs1111
+          Core Docs11
         </a>
       </li>
       <li>
         <a
           href="https://forum.vuejs.org"
           target="_blank"
+          rel ="noopener noreferrer"
         >
-          Forum1111
+          Forum11
         </a>
       </li>
       <li>
         <a
           href="https://chat.vuejs.org"
           target="_blank"
+          rel ="noopener noreferrer"
         >
-          Community Chat1111
+          Community Chat11
         </a>
       </li>
       <li>
         <a
           href="https://twitter.com/vuejs"
           target="_blank"
+          rel ="noopener noreferrer"
         >
           Twitter
         </a>
@@ -40,6 +44,7 @@
         <a
           href="http://vuejs-templates.github.io/webpack/"
           target="_blank"
+          rel ="noopener noreferrer"
         >
           Docs for This Template
         </a>
@@ -51,6 +56,7 @@
         <a
           href="http://router.vuejs.org/"
           target="_blank"
+          rel ="noopener noreferrer"
         >
           vue-router
         </a>
@@ -59,6 +65,7 @@
         <a
           href="http://vuex.vuejs.org/"
           target="_blank"
+          rel ="noopener noreferrer"
         >
           vuex
         </a>
@@ -67,6 +74,7 @@
         <a
           href="http://vue-loader.vuejs.org/"
           target="_blank"
+          rel ="noopener noreferrer"
         >
           vue-loader
         </a>
@@ -75,6 +83,7 @@
         <a
           href="https://github.com/vuejs/awesome-vue"
           target="_blank"
+          rel ="noopener noreferrer"
         >
           awesome-vue
         </a>
